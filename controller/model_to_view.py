e94b341ef97e967397eb75f6ab56f504ca0a3a8c
sydleither <sydleither@protonmail.com>
2021-06-26 18:46:51 -0400

    progress on grocery list again

--- controller/model_to_view.py
+++ controller/model_to_view.py
@@ -33,6 +33,7 @@ def single_recipe(recipe_id):
 def grocery_list():
     planner = get_planner_recipe_ids()
     recipes = [x for meal in planner for x in meal]
+    measurements = create_measurement_dict()
 
     groceries = {}
     for recipe in recipes:
@@ -41,49 +42,64 @@ def grocery_list():
             ingredients = get_recipe_ingredients_with_ids(recipe)
             for ingredient in ingredients:
                 if ingredient[0] in groceries:
-                    new_grocery = add_measurements(ingredient[0], \
+                    new_grocery = add_measurements(measurements, ingredient[1], \
                                                    groceries[ingredient[0]][1], \
                                                    groceries[ingredient[0]][2], \
-                                                   ingredient[1], ingredient[2])
+                                                   ingredient[2], ingredient[3])
                     groceries[ingredient[0]] = new_grocery
                 else:
                     groceries[ingredient[0]] = [ingredient[1], ingredient[2], ingredient[3]]
         except:
             if recipe != '' and recipe != 'Leftovers':
                 groceries[recipe] = []
+
+    return groceries
+
+
+def create_measurement_dict():
+    measurements = {}
+    measurements[1] = [['gallon'], 16]
+    measurements[3] = [['quart'], 4]
+    measurements[4] = [['pint'], 2]
+    measurements[2] = [['cup', 'c', 'cups'], 1]
+    measurements[5] = [['oz', 'ounce', 'ounces'], .125]
+    measurements[6] = [['tbsp', 'tablespoon', 'tablespoons'], .0625]
+    measurements[7] = [['tsp', 'teaspoon', 'teaspoons'], .0208]
+    return measurements
+
     
-    
-def add_measurements(name, q1, m1, q2, m2):
-    measurements = {} #TODO read in measurements dict
-    m1_cat = ''
-    m2_cat = ''
+def add_measurements(measurements, name, q1, m1, q2, m2):
+    m1_cat = 0
+    m2_cat = 0
     q_temp = 0
+    m_temp = ''
     final_quantity = 0
     final_measurement = ''
-    
-    if m1 == m2:
-        final_measurement = m1
-        q_temp = q1+q2
-    else:
-        for key,value in measurements.items():
-            if m1 in value[0]:
-                m1_cat = key
-            if m2 in value[0]:
-                m2_cat = key
-        if m1_cat != '' and m2_cat != '':
-            if m1_cat > m2_cat:
-                final_measurement = measurements[m1_cat][0]
-                q_temp = q2/(measurements[m1_cat]/measurements[m2_cat])
-            elif m2_cat < m1_cat:
-                final_measurement = measurements[m2_cat][0]
-                q_temp = q1/(measurements[m2_cat]/measurements[m1_cat])
-            else:
-                final_measurement = measurements[m1_cat][0]
-                q_temp = q1+q2
+
+    for key,value in measurements.items():
+        if m1 in value[0]:
+            m1_cat = key
+        if m2 in value[0]:
+            m2_cat = key
+    if m1_cat != 0 and m2_cat != 0:
+        if m1_cat > m2_cat:
+            m_temp = m1_cat
+            q_temp = q2/(measurements[m1_cat][1]/measurements[m2_cat][1])+q1
+        elif m2_cat < m1_cat:
+            m_temp = m2_cat
+            q_temp = q1/(measurements[m2_cat][1]/measurements[m1_cat][1])+q2
         else:
-            return #TODO deal with meausrements not in list
+            m_temp = m1_cat
+            q_temp = q1+q2
+    else:
+        return #TODO deal with meausrements not in list
 
     if m1 == m2 or (m1_cat != '' and m2_cat != ''):
-        return #TODO convert quantity to proper measurement
-        
+        q_in_cups = q_temp*measurements[m_temp][1]
+        for i in range(0,len(measurements)):
+            if q_in_cups == measurements[i+1][1]:
+                final_measurement = measurements[i+1][0][0]
+                final_quantity = q_in_cups/measurements[i+1][1]
+            #TODO check if between
+            
     return [name,final_quantity,final_measurement]
